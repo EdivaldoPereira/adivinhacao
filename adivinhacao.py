63159b43ed5d2b2f43db69eb81f366646780cb04
Edivaldo Pereira <edivaldo.pereira@gmail.com>
2022-04-04 18:02:18 -0300

    Alteração do if/else

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,10 +4,22 @@ print ("*********************************")
 
 numero_secreto = 42
 
-chute = input("Digite o seu número: ")
-print("Você digitou ", chute)
+chute_str = input("Digite o seu número: ")
 
-if (numero_secreto == int(chute)):
+print("Você digitou ", chute_str)
+
+chute = int(chute_str)
+
+acertou = numero_secreto == chute
+maior = chute > numero_secreto
+menor = chute < numero_secreto
+
+if (acertou):
     print("Você acertou")
 else:
-    print("Você errou")
+    if(maior):
+        print("Você errou! Seu chute foi maior que o número secreto.")
+    elif(menor):
+        print("Você error! Seu chute foi menor que o número secreto.")
+
+print("Fim de jogo")
