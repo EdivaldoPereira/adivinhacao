0058a3235036e2f2820eb839143e1c42a63cf324
Edivaldo Pereira <edivaldo.pereira@gmail.com>
2022-04-05 09:17:20 -0300

    Condicional if adicionada para limitar opções de escolha

--- adivinhacao.py
+++ adivinhacao.py
@@ -9,9 +9,13 @@ total_tentativas = 3
 #while(rodada <= total_tentativas):
 for rodada in range(1, total_tentativas + 1):
     print("Tentativa {} de {}.".format(rodada,total_tentativas))
-    chute_str = input("Digite o seu número: ")
+    chute_str = input("Digite um número entre 1 e 100: ")
     print("Você digitou ", chute_str)
     chute = int(chute_str)
+
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100")
+        continue
 
     acertou = numero_secreto == chute
     maior = chute > numero_secreto
