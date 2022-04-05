317e5961ad518e5293551765644f8c85668b9ac2
Edivaldo Pereira <edivaldo.pereira@gmail.com>
2022-04-05 07:56:13 -0300

    Inclusão do comando while

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,23 +3,25 @@ print("Bem-vindo ao jogo de Adivinhação!")
 print ("*********************************")
 
 numero_secreto = 42
+total_tentativas = 3
+rodada = 1
 
-chute_str = input("Digite o seu número: ")
+while(rodada <= total_tentativas):
+    print("Rodada ", rodada, " de ", total_tentativas)
+    chute_str = input("Digite o seu número: ")
+    print("Você digitou ", chute_str)
+    chute = int(chute_str)
 
-print("Você digitou ", chute_str)
+    acertou = numero_secreto == chute
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-chute = int(chute_str)
-
-acertou = numero_secreto == chute
-maior = chute > numero_secreto
-menor = chute < numero_secreto
-
-if (acertou):
-    print("Você acertou")
-else:
-    if(maior):
-        print("Você errou! Seu chute foi maior que o número secreto.")
-    elif(menor):
-        print("Você error! Seu chute foi menor que o número secreto.")
-
+    if (acertou):
+        print("Você acertou")
+    else:
+        if(maior):
+            print("Você errou! Seu chute foi maior que o número secreto.")
+        elif(menor):
+            print("Você errou! Seu chute foi menor que o número secreto.")
+    rodada = rodada + 1
 print("Fim de jogo")
